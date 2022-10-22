46fab7026bb1989bfc609361ef0673406ba75ec4
Lex <Lexor.V.A.S@yandex.ru>
2022-10-22 04:18:03 +0300

    add doker config 1.0

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -2,7 +2,7 @@ export default () => ({
   port: parseInt(process.env.PORT) || 3000,
   database: {
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST || 'host.docker.internal',
     port: parseInt(process.env.DATABASE_PORT) || 5432,
     username: 'student',
     password: '666777',
