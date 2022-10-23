ef1574ba6cdfa2b8d8106d558589cdf2886b3872
Lex <Lexor.V.A.S@yandex.ru>
2022-10-23 04:07:23 +0300

    config doker 1.3

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -4,7 +4,7 @@ export default () => ({
   port: parseInt(process.env.PORT) || 3000,
   database: {
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT) || 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
